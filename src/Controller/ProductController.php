8af800fb132485687b840046fbdc7e2c99afec03
Rayen <rayen@example.com>
2026-04-05 15:16:57 +0100

    Backup: current state before integrating TalentFlow

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\ProductRepository;
+
+final class ProductController extends AbstractController
+{
+    #[Route('/products', name: 'product_index')]
+    public function index(ProductRepository $repository): Response
+    {
+    
+  
+        return $this->render('product/index.html.twig', [
+            'products' => $repository->findAll(),
+        ]);
+    }
+}
